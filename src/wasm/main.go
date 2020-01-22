da04c2d3d75410339c7d11e04d4ab952023c02b5
Patrick Fiaux <pfiaux@Patricks-MacBook-Pro.local>
2020-01-22 08:06:17 +0100

    Adds dom interaction

--- src/wasm/main.go
+++ src/wasm/main.go
@@ -1,5 +1,19 @@
 package main
 
+import "syscall/js"
+
+func addTitle() {
+	document := js.Global().Get("document")
+	h1 := document.Call("createElement", "h1")
+	h1.Set("innerHTML", "Another World")
+	document.Get("body").Call("appendChild", h1)
+}
+
 func main() {
 	println("Hello Another-World")
+
+	addTitle()
+
+	channel := make(chan bool)
+	<-channel
 }
