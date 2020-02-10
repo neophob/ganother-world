e7216c2efa0649987f7e550466122068eba6d750
Michael Vogt <mvogt@control4.com>
2020-02-10 13:26:27 +0100

    moar comments

--- anotherworld/math.go
+++ anotherworld/math.go
@@ -1,9 +1,11 @@
 package anotherworld
 
+//ToUint16BE convert two bytes to an uint16 number, Big Endianess
 func ToUint16BE(lo, hi uint8) uint16 {
 	return uint16(hi) | uint16(lo)<<8
 }
 
+//ToUint32BE convert four bytes to an uint32 number, Big Endianess
 func ToUint32BE(b1, b2, b3, b4 uint8) uint32 {
 	return uint32(b4) | uint32(b3)<<8 | uint32(b2)<<16 | uint32(b1)<<24
 }
